f72fa4c8cb9e32dbe98c010b10d8d3424f4f42db
@johanbook <johanbook@hotmail.com>
2019-11-05 09:56:24 +0100

    Add confirmation to install script

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+echo "This will clear your existing configuration files."
+read -r -p "Are you sure you want to proceed? [y/N] " response
+if [ "$response" != "y" ]
+then
+    echo "Installation was cancelled by user"
+    exit 0
+fi
+
 pwd=$(pwd)
 
 # Vim
@@ -5,3 +13,6 @@ ln -sf $(pwd)/.vimrc ~/.vimrc
 ln -sf $(pwd)/vim/ ~/.vim
 
 ln -sf $(pwd)/.zshrc ~/.zshrc
+
+echo "Installation complete"
+echo "Restart all terminals for effects to take place"
