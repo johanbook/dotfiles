b1599a5646fb81fb9b66cb2bdf0627bf6b235077
Johan Book <johanbook@hotmail.com>
2021-08-21 10:59:31 +0200

    chore: update install script

--- install.sh
+++ install.sh
@@ -6,31 +6,17 @@ then
     exit 0
 fi
 
-# ctags
-apt-get install ctags
-ln -sf $(pwd)/.ctags ~/.ctags
-git config --global init.templatedir $(pwd)'/git_template'
-
-# i3
-ln -sf $(pwd)/i3/config ~/.i3/config
-ln -sf $(pwd)/i3/.i3blocks.conf ~/.i3blocks.conf
-ln -sf $(pwd)/i3/i3blocks/ ~/.config/i3/i3blocks
-
-# Vim
+# Configs
+ln -sf $(pwd)/alacritty/ ~/.config/alacritty
+ln -sf $(pwd)/.Rprofile ~/.Rprofile
+ln -sf $(pwd)/sway/ ~/.config/sway
 ln -sf $(pwd)/.vimrc ~/.vimrc
 ln -sf $(pwd)/vim/ ~/.vim
-
-# zsh
-ln -sf $(pwd)/.Rprofile ~/.Rprofile
+ln -sf $(pwd)/waybar/ ~/.config/waybar
 ln -sf $(pwd)/.zshrc ~/.zshrc
 
 echo "Installation complete"
 echo "Restart all terminals for effects to take place"
 
-# Spotify CLI
-# TODO: rename to spotify-cli. Should move file to bin
-# and remove remaining files
-git clone https://gist.github.com/fa6258f3ff7b17747ee3.git
-cd ./fa6258f3ff7b17747ee3
-chmod +x
-sudo ln -s ./sp /usr/local/bin/
+# Spotify client
+sudo pacman -S ncspot
