346a38fc6c094601912c814c39f8f8dc630d0ba7
Johan Book <johanbook@hotmail.com>
2021-11-10 16:44:33 +0100

    refactor(install): refactor our arch deps

--- install.sh
+++ install.sh
@@ -2,29 +2,10 @@
 
 set -e
 
-
 ################################################
 # Packages
 ################################################
-echo "- Installing Pacman packages. This will need sudo privileges"
-sudo pacman -S \
-	alacritty \
-	alsa-utils \
-	fzf \
-	git \
-	grim \
-	gvim \
-	make \
-	openssh \
-	ripgrep \
-	slurp \
-	sway \
-	swaylock \
-	ttf-font-awesome \
-	wayland \
-	waybar \
-	wofi \
-	zsh
+sh ./scripts/install.arch.sh
 
 echo "- Installing NVM (Nodejs Version Manager)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | sh
@@ -35,17 +16,15 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.dotfiles/b
 echo "- Installing Zsh theme Powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.dotfiles/bin/zsh-theme-powerlevel10k
 
-
 ###############################################
 # Configurations
 ################################################
 echo "- Symlinking configuration files"
 echo "This will clear your existing configuration files."
 read -r -p "Are you sure you want to proceed? [y/N] " response
-if [ "$response" != "y" ]
-then
-    echo "Installation was cancelled by user"
-    exit 0
+if [ "$response" != "y" ]; then
+	echo "Installation was cancelled by user"
+	exit 0
 fi
 
 mkdir -p ~/.config
@@ -58,12 +37,10 @@ ln -sf $(pwd)/waybar/ ~/.config/waybar
 ln -sf $(pwd)/.zshrc ~/.zshrc
 touch $(pwd)/.env
 
-
-
 ################################################
 # Cleanup
 ################################################
 echo "Installation complete"
 echo "Restart Sway and all terminals for effects to take place"
 echo "To complete vim installation, open Vim and run :PluginInstall"
-echo "To install Nodejs run `nvm install node && nvm use node`"
+echo "To install Nodejs run $(nvm install node && nvm use node)"
---
+++ scripts/install.arch.sh
@@ -0,0 +1,24 @@
+# This script will install Wayland, Sway and some other useful
+# Arch utilities
+
+set -e
+
+echo "- Installing Pacman packages. This will need sudo privileges"
+sudo pacman -S \
+	alacritty \
+	alsa-utils \
+	fzf \
+	git \
+	grim \
+	gvim \
+	make \
+	openssh \
+	ripgrep \
+	slurp \
+	sway \
+	swaylock \
+	ttf-font-awesome \
+	wayland \
+	waybar \
+	wofi \
+	zsh
